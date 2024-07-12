74566701d6e6ca52b686cd04f0fcd2de33e9c660
Nye <tnye@MacBook-Pro-2.local>
2024-07-11 18:07:51 -0700

    adjusted dataframe fmt

--- make_residual_file.py
+++ make_residual_file.py
@@ -15,10 +15,10 @@ Created on Wed Jul  3 08:33:00 2024
 import numpy as np
 import pandas as pd
 
-min_stns = 10
-min_evs = 10
+min_stns = 5
+min_evs = 5
 
-outfile = f'/Users/tnye/bayarea_path/files/residual_analysis/GMM_residuals_culled-{min_evs}evs-{min_stns}stns.csv'
+outfile = '/Users/tnye/bayarea_path/files/residual_analysis/GMM_residuals_culled.csv'
 
 # Read in IM flatfiles
 bssa14_rock_df = pd.read_csv('/Users/tnye/bayarea_path/files/residual_analysis/IM_flatfiles/IMs_BSSA14_rock.csv')
@@ -33,8 +33,8 @@ ask14_df = pd.read_csv('/Users/tnye/bayarea_path/files/residual_analysis/IM_flat
 stn_idx = np.array([])
 ev_idx = np.array([])
 
-events = np.unique(ask14_df['Event'])
-stns = np.unique(ask14_df['Station'])
+events = np.unique(bssa14_df['Event'])
+stns = np.unique(bssa14_df['Station'])
 
 for stn in stns:
     idx = np.where(ask14_df['Station'] == stn)[0]
@@ -48,10 +48,10 @@ for ev in events:
 
 useable_idx = np.intersect1d(stn_idx,ev_idx)
 
-bssa14_rock_df = bssa14_rock_df.loc[useable_idx]
-bssa14_df = bssa14_df.loc[useable_idx]
-ask14_rock_df = ask14_rock_df.loc[useable_idx]
-ask14_df = ask14_df.loc[useable_idx]
+bssa14_rock_df = bssa14_rock_df.loc[useable_idx].reset_index()
+bssa14_df = bssa14_df.loc[useable_idx].reset_index()
+ask14_rock_df = ask14_rock_df.loc[useable_idx].reset_index()
+ask14_df = ask14_df.loc[useable_idx].reset_index()
 
 
 #%%
@@ -103,45 +103,46 @@ pgm_res_dict = {'BSSA14rock_PGA_res':bssa14_pga_rock_res,'BSSA14_PGA_res':bssa14
 pgm_res_df = pd.DataFrame(pgm_res_dict)
 
 # BSSA14 SA rock residual columns
-bssa14rock_SA_col_headers = ['BSSA14rock_SA_res(T=0.01)(g)','BSSA14rock_SA_res(T=0.02)(g)','BSSA14rock_SA_res(T=0.03)(g)',
-                  'BSSA14rock_SA_res(T=0.05)(g)','BSSA14rock_SA_res(T=0.075)(g)','BSSA14rock_SA_res(T=0.1)(g)',
-                  'BSSA14rock_SA_res(T=0.15)(g)','BSSA14rock_SA_res(T=0.2)(g)','BSSA14rock_SA_res(T=0.25)(g)',
-                  'BSSA14rock_SA_res(T=0.3)(g)','BSSA14rock_SA_res(T=0.4)(g)','BSSA14rock_SA_res(T=0.5)(g)',
-                  'BSSA14rock_SA_res(T=0.75)(g)','BSSA14rock_SA_res(T=1.0)(g)','BSSA14rock_SA_res(T=1.5)(g)',
-                  'BSSA14rock_SA_res(T=2.0)(g)','BSSA14rock_SA_res(T=3.0)(g)','BSSA14rock_SA_res(T=4.0)(g)',
-                  'BSSA14rock_SA_res(T=5.0)(g)','BSSA14rock_SA_res(T=7.5)(g)','BSSA14rock_SA_res(T=10.0)(g)']
+bssa14rock_SA_col_headers = ['BSSA14rock_SA_res_T0.01','BSSA14rock_SA_res_T0.02','BSSA14rock_SA_res_T0.03',
+                  'BSSA14rock_SA_res_T0.05','BSSA14rock_SA_res_T0.075','BSSA14rock_SA_res_T0.1',
+                  'BSSA14rock_SA_res_T0.15','BSSA14rock_SA_res_T0.2','BSSA14rock_SA_res_T0.25',
+                  'BSSA14rock_SA_res_T0.3','BSSA14rock_SA_res_T0.4','BSSA14rock_SA_res_T0.5',
+                  'BSSA14rock_SA_res_T0.75','BSSA14rock_SA_res_T1.0','BSSA14rock_SA_res_T1.5',
+                  'BSSA14rock_SA_res_T2.0','BSSA14rock_SA_res_T3.0','BSSA14rock_SA_res_T4.0',
+                  'BSSA14rock_SA_res_T5.0','BSSA14rock_SA_res_T7.5','BSSA14rock_SA_res_T10.0']
 bssa14rock_SA_df = pd.DataFrame(bssa14_SA_rock_res, columns=bssa14rock_SA_col_headers)
      
 # BSSA14 SA residual columns           
-bssa14_SA_col_headers = ['BSSA14_SA_res(T=0.01)(g)','BSSA14_SA_res(T=0.02)(g)','BSSA14_SA_res(T=0.03)(g)',
-                  'BSSA14_SA_res(T=0.05)(g)','BSSA14_SA_res(T=0.075)(g)','BSSA14_SA_res(T=0.1)(g)',
-                  'BSSA14_SA_res(T=0.15)(g)','BSSA14_SA_res(T=0.2)(g)','BSSA14_SA_res(T=0.25)(g)',
-                  'BSSA14_SA_res(T=0.3)(g)','BSSA14_SA_res(T=0.4)(g)','BSSA14_SA_res(T=0.5)(g)',
-                  'BSSA14_SA_res(T=0.75)(g)','BSSA14_SA_res(T=1.0)(g)','BSSA14_SA_res(T=1.5)(g)',
-                  'BSSA14_SA_res(T=2.0)(g)','BSSA14_SA_res(T=3.0)(g)','BSSA14_SA_res(T=4.0)(g)',
-                  'BSSA14_SA_res(T=5.0)(g)','BSSA14_SA_res(T=7.5)(g)','BSSA14_SA_res(T=10.0)(g)']
+bssa14_SA_col_headers = ['BSSA14_SA_res_T0.01','BSSA14_SA_res_T0.02','BSSA14_SA_res_T0.03',
+                  'BSSA14_SA_res_T0.05','BSSA14_SA_res_T0.075','BSSA14_SA_res_T0.1',
+                  'BSSA14_SA_res_T0.15','BSSA14_SA_res_T0.2','BSSA14_SA_res_T0.25',
+                  'BSSA14_SA_res_T0.3','BSSA14_SA_res_T0.4','BSSA14_SA_res_T0.5',
+                  'BSSA14_SA_res_T0.75','BSSA14_SA_res_T1.0','BSSA14_SA_res_T1.5',
+                  'BSSA14_SA_res_T2.0','BSSA14_SA_res_T3.0','BSSA14_SA_res_T4.0',
+                  'BSSA14_SA_res_T5.0','BSSA14_SA_res_T7.5','BSSA14_SA_res_T10.0']
 bssa14_SA_df = pd.DataFrame(bssa14_SA_res, columns=bssa14_SA_col_headers)
 
 # ASK14 SA rock residual columns
-ask14rock_SA_col_headers = ['ASK14rock_SA_res(T=0.01)(g)','ASK14rock_SA_res(T=0.02)(g)','ASK14rock_SA_res(T=0.03)(g)',
-                  'ASK14rock_SA_res(T=0.05)(g)','ASK14rock_SA_res(T=0.075)(g)','ASK14rock_SA_res(T=0.1)(g)',
-                  'ASK14rock_SA_res(T=0.15)(g)','ASK14rock_SA_res(T=0.2)(g)','ASK14rock_SA_res(T=0.25)(g)',
-                  'ASK14rock_SA_res(T=0.3)(g)','ASK14rock_SA_res(T=0.4)(g)','ASK14rock_SA_res(T=0.5)(g)',
-                  'ASK14rock_SA_res(T=0.75)(g)','ASK14rock_SA_res(T=1.0)(g)','ASK14rock_SA_res(T=1.5)(g)',
-                  'ASK14rock_SA_res(T=2.0)(g)','ASK14rock_SA_res(T=3.0)(g)','ASK14rock_SA_res(T=4.0)(g)',
-                  'ASK14rock_SA_res(T=5.0)(g)','ASK14rock_SA_res(T=7.5)(g)','ASK14rock_SA_res(T=10.0)(g)']
+ask14rock_SA_col_headers = ['ASK14rock_SA_res_T0.01','ASK14rock_SA_res_T0.02','ASK14rock_SA_res_T0.03',
+                  'ASK14rock_SA_res_T0.05','ASK14rock_SA_res_T0.075','ASK14rock_SA_res_T0.1',
+                  'ASK14rock_SA_res_T0.15','ASK14rock_SA_res_T0.2','ASK14rock_SA_res_T0.25',
+                  'ASK14rock_SA_res_T0.3','ASK14rock_SA_res_T0.4','ASK14rock_SA_res_T0.5',
+                  'ASK14rock_SA_res_T0.75','ASK14rock_SA_res_T1.0','ASK14rock_SA_res_T1.5',
+                  'ASK14rock_SA_res_T2.0','ASK14rock_SA_res_T3.0','ASK14rock_SA_res_T4.0',
+                  'ASK14rock_SA_res_T5.0','ASK14rock_SA_res_T7.5','ASK14rock_SA_res_T10.0']
 ask14rock_SA_df = pd.DataFrame(ask14_SA_rock_res, columns=ask14rock_SA_col_headers)
 
 # ASK14 SA residual columns
-ask14_SA_col_headers = ['ASK14_SA_res(T=0.01)(g)','ASK14_SA_res(T=0.02)(g)','ASK14_SA_res(T=0.03)(g)',
-                  'ASK14_SA_res(T=0.05)(g)','ASK14_SA_res(T=0.075)(g)','ASK14_SA_res(T=0.1)(g)',
-                  'ASK14_SA_res(T=0.15)(g)','ASK14_SA_res(T=0.2)(g)','ASK14_SA_res(T=0.25)(g)',
-                  'ASK14_SA_res(T=0.3)(g)','ASK14_SA_res(T=0.4)(g)','ASK14_SA_res(T=0.5)(g)',
-                  'ASK14_SA_res(T=0.75)(g)','ASK14_SA_res(T=1.0)(g)','ASK14_SA_res(T=1.5)(g)',
-                  'ASK14_SA_res(T=2.0)(g)','ASK14_SA_res(T=3.0)(g)','ASK14_SA_res(T=4.0)(g)',
-                  'ASK14_SA_res(T=5.0)(g)','ASK14_SA_res(T=7.5)(g)','ASK14_SA_res(T=10.0)(g)']
+ask14_SA_col_headers = ['ASK14_SA_res_T0.01','ASK14_SA_res_T0.02','ASK14_SA_res_T0.03',
+                  'ASK14_SA_res_T0.05','ASK14_SA_res_T0.075','ASK14_SA_res_T0.1',
+                  'ASK14_SA_res_T0.15','ASK14_SA_res_T0.2','ASK14_SA_res_T0.25',
+                  'ASK14_SA_res_T0.3','ASK14_SA_res_T0.4','ASK14_SA_res_T0.5',
+                  'ASK14_SA_res_T0.75','ASK14_SA_res_T1.0','ASK14_SA_res_T1.5',
+                  'ASK14_SA_res_T2.0','ASK14_SA_res_T3.0','ASK14_SA_res_T4.0',
+                  'ASK14_SA_res_T5.0','ASK14_SA_res_T7.5','ASK14_SA_res_T10.0']
 ask14_SA_df = pd.DataFrame(ask14_SA_res, columns=ask14_SA_col_headers)
 
 # Final dataframe
 res_df = main_df.join(pgm_res_df.join(bssa14rock_SA_df.join(bssa14_SA_df.join(ask14rock_SA_df.join(ask14_SA_df)))))
 res_df.to_csv(outfile)
+
